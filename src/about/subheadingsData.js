13bde7ae8ae6d878a8f3f660839833e4e754fcee
krissyweber <122844703+krissyweber@users.noreply.github.com>
2024-01-27 21:41:46 -0500

    about me

--- src/about/subheadingsData.js
+++ src/about/subheadingsData.js
@@ -5,7 +5,9 @@ const subheadingsData = {
             content: (
                 <>
                 <p>
-                    My name is Kristine blah blah
+                I'm a full-stack developer with a passion for more than just coding. My love for animals and the outdoors is a big part of who I am. Away from the computer, I enjoy exploring nature and volunteering with animals. These interests keep me grounded and inspire my work in technology.
+
+Balancing screen time with outdoor activities helps me stay creative and adaptable. Caring for animals has taught me empathy and patience. I believe these qualities are just as important in the tech world as they are in nature. I bring this balanced approach to my professional life, aiming to create tech solutions that are both innovative and user-friendly.
                 </p>
                 </>
             ),
@@ -16,8 +18,10 @@ const subheadingsData = {
         title: "2023 Diploma",
         content: (
             <>
+            <p>Computer Programming Diploma</p>
+            <p>Algonquin College, 2023</p>
             <p>
-                My name is Kristine blah blah
+            Successfully completed a comprehensive diploma program in Computer Programming at Algonquin College. This program equipped me with a solid foundation in various programming languages, software development, and problem-solving skills. Key areas of study included web development, database management, and software design principles, all of which have provided me with the tools necessary to excel in the fast-paced and evolving field of technology. This educational experience not only honed my technical abilities but also emphasized the importance of teamwork, effective communication, and continuous learning in the world of computer programming.  
             </p>
             </>
         ),
